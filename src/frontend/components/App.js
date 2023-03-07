eb7308a15249d9977494effad58e94f9188ee531
Robel Gebremeskel <rofikre@yahoo.com>
2023-03-07 17:12:38 -0500

    Added function to connect to blockchain & load contract

--- src/frontend/components/App.js
+++ src/frontend/components/App.js
@@ -1,8 +1,47 @@
+import logo from "./logo.png";
+import "./App.css";
+import MarketplaceAbi from "../contractsData/Marketplace.json";
+import MarketplaceAddress from "../contractsData/Marketplace-address.json";
+import NFTAbi from "../contractsData/NFT.json";
+import NFTAddress from "../contractsData/NFT-address.json";
+import { useState } from "react";
 
-import logo from './logo.png';
-import './App.css';
- 
+import { ethers } from "ethers";
+
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [account, setAccount] = useState(null);
+  const [nft, setNFT] = useState({});
+  const [marketplace, setMarketplace] = useState({});
+
+  // MethaMask Login/Connect
+  const web3Handler = async () => {
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts"
+    });
+    setAccount(accounts[0]);
+
+    // Get provider from Metamask
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    // Set signer
+    const signer = provider.getSigner();
+
+    loadContracts(signer);
+  };
+
+  const loadContracts = async (signer) => {
+    // Get deployed copies
+    const marketplace = new ethers.Contract(
+      MarketplaceAddress.address,
+      MarketplaceAbi.abi,
+      signer
+    );
+    setMarketplace(marketplace);
+    const nft = new ethers.Contract(NFTAddress.address, NFTAbi.abi, signer);
+    setNFT(nft);
+    setLoading(false);
+  };
+
   return (
     <div>
       <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
@@ -24,11 +63,12 @@ function App() {
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <img src={logo} className="App-logo" alt="logo"/>
+                <img src={logo} className="App-logo" alt="logo" />
               </a>
-              <h1 className= "mt-5">Dapp University Starter Kit</h1>
+              <h1 className="mt-5">Dapp University Starter Kit</h1>
               <p>
-                Edit <code>src/frontend/components/App.js</code> and save to reload.
+                Edit <code>src/frontend/components/App.js</code> and save to
+                reload.
               </p>
               <a
                 className="App-link"
@@ -36,7 +76,10 @@ function App() {
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                LEARN BLOCKCHAIN <u><b>NOW! </b></u>
+                LEARN BLOCKCHAIN{" "}
+                <u>
+                  <b>NOW! </b>
+                </u>
               </a>
             </div>
           </main>
